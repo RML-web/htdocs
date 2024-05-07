97d065a781425bba3cc16d6c755bb5801a3ee912
Ram <rmldesarrolloweb@gmail.com>
2024-05-07 14:22:03 +0200

    ccc

--- 12_arrays/index.php
+++ 12_arrays/index.php
@@ -21,4 +21,35 @@ foreach ($cantantes as $cantante) {
 }
 echo "</ul>";
 
+//Arrays asociativos
+$personas = array (
+    'nombre' => 'Mario',
+    'apellidos' => 'Bross',
+    'trabajo' => 'Fontanero'
+
+);
+
+var_dump($personas);
+
+echo = $personas['nombre'];
+echo "<hr>";
+//recorrer array asociativo
+foreach($persona as $key => $value) {
+    echo $key .' : ' . $value . '<br>';
+}
+
+//Array multidimensional
+$contactos = [
+
+    ['nombre'=> "Bart", 'email' => 'bart@email.com'],
+    ['nombre'=> "Lisa", 'email' => 'Lisa@email.com'],
+    ['nombre'=> "Maggie", 'email' => 'Maggiet@email.com'],
+];
+
+var_ dump($contactos);
+echo "<hr>";
+foreach($contactos as $key => $contacto) {
+    echo $key . ': El email de ' . $contacto['nombre'] . 'es' . $contacto['email'] . '<br>'
+}
+
 
