59b8dac8c7cb93bb47c6c1a8c3c6d003f9050ff0
Ram <rmldesarrolloweb@gmail.com>
2024-05-09 14:39:53 +0200

    fich

---
+++ Más_ejercicios_php/16_ficheros/index.php
@@ -0,0 +1,18 @@
+<?php
+//Abrir archivo
+$archivo = fopen("archivo.txt", "a+");
+
+var_dump($archivo);
+//leer hasta el final
+
+while (!feof($archivo)){
+    $contenido = fgets($archivo);
+
+echo  '<p>'. $contenido . '</p>';
+
+}
+
+
+$contenido = fgets($archivo);
+
+echo $contenido;
