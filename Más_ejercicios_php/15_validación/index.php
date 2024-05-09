0ba2e8086abff15fd1cf57ea5184679cdb5cc693
Ram <rmldesarrolloweb@gmail.com>
2024-05-09 14:11:38 +0200

    tamara

--- Más_ejercicios_php/15_validación/index.php
+++ Más_ejercicios_php/15_validación/index.php
@@ -32,31 +32,67 @@
 
 <body>
     <?php
-    function Filter($data)
+    function filter($data)
     {
+
         $data = trim($data);
-        $data = stripcslashes($data);
+        $data = stripslashes($data); //quitar barras \
         $data = htmlspecialchars($data);
         return $data;
     }
-    
-    
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['sumbit'])) {
+        $errores = [];
+        if (empty($_POST['nombre']) || preg_match("/[0-9]/", $_POST['nombre'])) {
+            $errores[] = "El nombre no puede estar vacio y no puede contener números.";
+        }
+        if (empty($_POST['password']) || strlen($_POST['password']) < 4) {
+            $errores[] = "El campo de la contraseña no puede estar vacio y ser mayor de 4.";
+        }
+        if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errores[] = "El campo de email no puede estar vacio y debe ser un email valido.";
+        }
+        if (empty($_POST['web']) || !filter_var($_POST['web']. FILTER_VALIDATE_URL)) {
+            $errores[] = "El campo de web no le importa a nadie pero no puede estar vacio y debe ser válido.";
+        }
 
-        var_dump($_POST);
-        $nombre = filter($_POST['nombre']);
-        $password = filter($_POST['password']);
-        $estudios = filter($_POST['estudios']);
-        $nacionalidad = filter($_POST['nacionalidad']);
-        $_POST["idiomas"];
-        
-        
-}
+        if (empty($errores)) {
+
+            $nombre = filter($_POST['nombre']);
+            $password = filter($_POST['password']);
+            $estudios = filter($_POST['estudios']);
+            $nacionalidad = filter($_POST['nacionalidad']);
+            $idiomas = filter(implode(", ", $_POST['idiomas']));
+            $web = filter($_POST['web']);
+            $email = filter($_POST['email']);
+            echo $nombre;
+        }
+    }
     ?>
+    <ul>
+        <?php if(isset($errores)){
+            foreach($errores as $error){
+                echo '<li>'.$error.'</li>';
+            }
+        }
+        ?>
+    </ul>
+
+    <?php if (isset($_POST['sumbit'])) : ?>
+        <h2>Has enviado los siguientes datos</h2>
+        <p>Nombre: <?php isset($nombre) ? print $nombre : "" ?></p>
+        <p>Password: <?php isset($password) ? print $password : "" ?></p>
+        <p>Estudios: <?php isset($estudios) ? print $estudios : "" ?></p>
+        <p>Nacionalidad: <?php isset($nacionalidad) ? print $nacionalidad : "" ?></p>
+        <p>Idiomas: <?php isset($idiomas) ? print $idiomas : "" ?></p>
+        <p>Web: <?php isset($web) ? print $web : "" ?></p>
+        <p>Email: <?php isset($email) ? print $email : "" ?></p>
+    <?php endif; ?>
+
+
     <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div>
             <label for="nombre">Nombre</label>
-            <input type="text" name="nombre" maxlength="20" id="nombre">
+            <input type="text" name="nombre" maxlength="50" id="nombre">
         </div>
         <div>
             <label for="password">Contraseña</label>
@@ -73,7 +109,7 @@
         </div>
         <div>
             <fieldset>
-                <legend for="nacionalidad">Nacionalidad</legend>
+                <legend>Nacionalidad</legend>
                 <div>
                     <input type="radio" name="nacionalidad" id="es" value="es" checked>
                     <label for="es"> Española</label>
@@ -87,8 +123,8 @@
         <fieldset>
             <legend>Idiomas:</legend>
             <div>
-                <input type="checkbox" name="idiomas[]" value="Castellano" id="es" checked>
-                <label for="es">Español</label>
+                <input type="checkbox" name="idiomas[]" value="Castellano" id="esp" checked>
+                <label for="esp">Español</label>
             </div>
             <div>
                 <input type="checkbox" name="idiomas[]" value="catalan" id="cat">
@@ -115,4 +151,5 @@
 
     </form>
 </body>
+
 </html>
