df1a7f26918b4ebe7d998a7218f5f1c89c1c3a40
Ram <rmldesarrolloweb@gmail.com>
2024-05-10 14:30:51 +0200

    botones

---
+++ Proyecto_registro-main/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Botones</title>
+    <style>
+
+.button1,  .button2{
+    background-color: pink;
+  color: white;
+  height: 20%;
+  width: 20%;
+
+  text-align: center;
+  border
+  display: inline-block;
+  font-size: 16px;
+  border: violet 20px solid;
+  margin: auto;
+ display: flex;
+ justify-content: flex-end;
+ text-align: center;
+
+
+}
+.button1 {
+    margin-top: 20%;
+}
+.button2 {
+    margin-botom: 0;
+}
+body {
+    max-widht: 100vw;
+
+
+}
+
+    </style>
+</head>
+<body>
+
+<button class="button1"><a href="login.php">Login</a></button>
+<button class="button2"><a href="registro.php">Registro</a></button>
+
+
+    
+</body>
+</html>
